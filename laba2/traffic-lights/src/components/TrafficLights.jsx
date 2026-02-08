a4c3a75b8e4a8b1323fa3704f04f4f629507face
Andriy Vlonha <andriy.vlonha.dev@gmail.com>
2026-02-08 15:22:57 +0200

    style: додано реалістичні темні відтінки для неактивних світел

--- laba2/traffic-lights/src/components/TrafficLights.jsx
+++ laba2/traffic-lights/src/components/TrafficLights.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react";
 import Light from "./Light";
 import PropTypes from "prop-types";
 
 const TrafficLights = ({ orientation = "vertical" }) => {
+  const [activeLight, setActiveLight] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveLight((prev) => (prev + 1) % 3);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const containerStyle = {
     display: "flex",
     flexDirection: orientation === "vertical" ? "column" : "row",
@@ -15,9 +26,9 @@ const TrafficLights = ({ orientation = "vertical" }) => {
 
   return (
     <div style={containerStyle}>
-      <Light tlColor="red" />
-      <Light tlColor="yellow" />
-      <Light tlColor="green" />
+      <Light tlColor="red" isActive={activeLight === 0} />
+      <Light tlColor="yellow" isActive={activeLight === 1} />
+      <Light tlColor="green" isActive={activeLight === 2} />
     </div>
   );
 };
